59f538260b51dacaa3269e3c883c08291ae168f9
JacobShiber <yaakovshiber@gmail.com>
2022-01-20 17:08:12 +0200

    Add project files.

---
+++ SportShop-Program/Controllers/ShoesController.cs
@@ -0,0 +1,32 @@
+﻿using SportShop_Program.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportShop_Program.Controllers
+{
+    public class ShoesController : Controller
+    {
+        SportShopContextDataContext dataContext = new SportShopContextDataContext();
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            List<Shoe> shoesList = dataContext.Shoes.ToList();
+            return View(shoesList);
+        }
+
+        public ActionResult GetSales()
+        {
+            List<Shoe> shoesInSale = dataContext.Shoes.Where(shoe => shoe.IsInSale == true).ToList();
+            return View(shoesInSale);
+        }
+
+        public ActionResult GetAdminView()
+        {
+            List<Shoe> shoesList = dataContext.Shoes.ToList();
+            return View(shoesList);
+        }
+    }
+}
---
+++ SportShop-Program/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportShop_Program.MvcApplication" Language="C#" %>
